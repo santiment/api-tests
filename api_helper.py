4ae42b85d82a02ace0d10060ef7102f90406bd23
mtkachuk <maksim.tkachuk@gmail.com>
2020-01-10 12:18:35 +0300

    Fix empty result processing for histogram metrics, add exception for unknown queries

--- api_helper.py
+++ api_helper.py
@@ -44,11 +44,9 @@ def get_query_data(query, slug, dt_from, dt_to, interval):
         response = execute_gql(gql_query)
         return response[query]
     elif query in special_queries:
-        print(f"Query {query} is used in other format.")
-        return
+        raise SanError(f"Query {query} is used in other format.")
     else:
-        print(f"Unknown query: {query}")
-        return
+        raise SanError(f"Unknown query: {query}")
 
 
 def get_timeseries_metric_data(metric, slug, dt_from, dt_to, interval):
@@ -94,7 +92,7 @@ def get_histogram_metric_data(metric, slug, dt_from, dt_to, interval, limit):
     }
     '''
     response = execute_gql(gql_query)
-    return response['getMetric']['histogramData']
+    return response['getMetric']['histogramData']['values']['data']
 
 
 if __name__ == '__main__':
