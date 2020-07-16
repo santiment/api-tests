e4ff7ca306b04f5fefaaf7d326142e873c942cda
Vassil Kalkov <103871+kalkov@users.noreply.github.com>
2020-07-16 16:43:33 +0300

    Address linter issues for MetricReport

--- metric_report.py
+++ metric_report.py
@@ -8,7 +8,7 @@ class MetricReport:
         self.slug = slug
         self.query = query
         self.status = None
-        self.error = None
+        self.error = {}
         self.error_details = []
 
     def set_empty(self):
@@ -21,19 +21,18 @@ class MetricReport:
         self.status = 'GraphQL error'
 
     def is_corrupted(self):
-        self.error and self.status == 'corrupted'
+        return self.error is not None and self.status == 'corrupted'
 
     def has_errors(self):
-        if self.status is not None:
-            return True
-        else:
-            return False
+        return bool(self.status)
 
     def error_output(self):
         if self.status == 'GraphQL error':
-            return 'graphql error'
+            message = 'graphql error'
         elif self.status == 'empty' or self.status == 'corrupted':
-            return 'corrupted data'
+            message = 'corrupted data'
+
+        return message
 
     def set_error_details(self, details):
         self.error['details'] = details
@@ -44,12 +43,12 @@ class MetricReport:
         return first_part + second_part
 
     def error_to_json(self):
-        return {
-            'metric': self.metric,
-            'reason': self.status,
-            'gql_query': self.query,
-            'gql_query_url': self.generate_gql_url()
-        }
+        self.error['metric'] = self.metric
+        self.error['reason'] = self.status
+        self.error['gql_query'] = self.query
+        self.error['gql_query_url'] = self.generate_gql_url()
+
+        return self.error
 
     def summary_to_json(self, ignored_metrics_key):
         json_summary = {}
@@ -58,8 +57,9 @@ class MetricReport:
         else:
             json_summary = {'name': self.metric, 'status': 'passed'}
 
+        ignored_metrics_list = ignored_metrics[self.slug][ignored_metrics_key]
 
-        if ignored_metrics and self.slug in ignored_metrics and self.metric in ignored_metrics[self.slug][ignored_metrics_key]:
+        if ignored_metrics and self.slug in ignored_metrics and self.metric in ignored_metrics_list:
             json_summary = {'name': self.metric, 'status': 'ignored'}
 
         return json_summary
